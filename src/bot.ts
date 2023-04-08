ba324f7f519e3a2489a01a7d4963750c1b7eb191
Vlad Gurbin <9560482@stud.op.edu.ua>
2023-04-08 16:06:24 +0300

    moved GemMinerBot class from index to bot file

---
+++ src/bot.ts
@@ -0,0 +1,36 @@
+import {Bot, CommandContext, Context} from "grammy";
+import {randomInteger} from "./utilities.js";
+
+export class GemMinerBot {
+    private bot: Bot;
+
+    constructor(token: string) {
+        this.bot = new Bot(token);
+        this.bot.command('mine', this.handleMineCommand.bind(this));
+        console.log('Bot created');
+    }
+
+    public botStart(): void{
+        this.bot.start();
+        console.log('Bot started');
+    }
+    private handleMineCommand = (ctx: CommandContext<Context>): void => {
+        if(ctx.message === undefined){
+            return;
+        }
+        const gems = randomInteger(-5, 10);
+        const username = `@${ctx.message.from.username}`;
+        const word = gems === 1 || gems === -1 ? 'gem' : 'gems';
+        let message: string;
+        if (gems < 0) {
+            const lostGems = -gems;
+            message = `${username}, you lost ${lostGems} ${word}...`;
+        } else if (gems === 0) {
+            message = `${username}, you got nothing.`;
+        } else {
+            message = `${username}, you got ${gems} ${word}!`;
+        }
+        ctx.reply(message);
+        console.log(`${username}: ${gems}`);
+    };
+}
--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,5 @@
-import { Bot, Context } from 'grammy';
-import { randomInteger } from "./utilities.js";
-import { TOKEN } from "./token.js";
-
-class GemMinerBot {
-    private bot: Bot;
-
-    constructor(token: string) {
-        this.bot = new Bot(token);
-        this.bot.command('mine', this.handleMineCommand.bind(this));
-        this.bot.start();
-        console.log('Bot created');
-    }
-
-    private handleMineCommand = (ctx: Context): void => {
-        const gems = randomInteger(-5, 10);
-        const username = `@${ctx.message?.from.username}`;
-        const word = gems === 1 ? 'gem' : 'gems';
-
-        if (gems < 0) {
-            const lostGems = -gems;
-            const message = `${username}, you lost ${lostGems} ${word}...`;
-            ctx.reply(message);
-            console.log(`${username}: ${lostGems} ${word} lost`);
-        } else if (gems === 0) {
-            const message = `${username}, you got nothing.`;
-            ctx.reply(message);
-            console.log(`${username}: nothing`);
-        } else {
-            const message = `${username}, you got ${gems} ${word}!`;
-            ctx.reply(message);
-            console.log(`${username}: ${gems} ${word} mined`);
-        }
-    };
-}
+import {TOKEN} from "./token.js"
+import {GemMinerBot} from "./bot.js"
 
 const bot = new GemMinerBot(TOKEN);
+bot.botStart();
