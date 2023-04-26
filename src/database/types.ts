d18af694dc9944134ddec79963b0187217153218
Vlad Gurbin <9560482@stud.op.edu.ua>
2023-04-26 09:37:13 +0300

    Added fields for lvl

--- src/database/types.ts
+++ src/database/types.ts
@@ -2,6 +2,9 @@ interface Player {
     userId: number
     chatId: number
     heroName : string
+      playerLevel: number
+      expCount: number
+      newExp: number
     gemsCount: number
     moneyCount: number
     lastMined: Date
